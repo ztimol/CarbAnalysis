833b8dd4d19c9944cb97989feb7191eb6aa32739
Zaheer <ztimol@gmail.com>
2023-08-17 19:31:01 +0200

    fix to env in settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.exists(os.path.join(BASE_DIR, ".env")):
+    from dotenv import load_dotenv
+
+    load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
